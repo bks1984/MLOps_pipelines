904ac31376b3365ff0ec72d9ed013c13f9ee2c59
biplob <110578485+bks1984@users.noreply.github.com>
2026-02-15 17:58:52 +0530

    Update hosting.py

--- mlops-pipelines/hosting/hosting.py
+++ mlops-pipelines/hosting/hosting.py
@@ -5,9 +5,8 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 
 api.upload_folder(
     folder_path="mlops-pipelines/deployment",
-    repo_id="bipjack/ml-healthcare",
+    repo_id="bipjack/PIMA-Diabetes-Prediction",
     repo_type="space",
-    path_in_repo="."
+    path_in_repo=".",
 )
-
 print("Deployment pushed successfully.")
