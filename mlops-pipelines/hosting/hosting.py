db26d9e98ea5da29c8574e29262fe344083e41d5
biplob <110578485+bks1984@users.noreply.github.com>
2026-02-15 17:43:24 +0530

    Update hosting.py

--- mlops-pipelines/hosting/hosting.py
+++ mlops-pipelines/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="mlops-pipelines/deployment",
-    repo_id="bipjack/PIMA-Diabetes-Prediction"                                         # enter the Hugging Face username here
+    repo_id="bipjack/PIMA-Diabetes-Prediction",                                         # enter the Hugging Face username here
     repo_type="space",
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
