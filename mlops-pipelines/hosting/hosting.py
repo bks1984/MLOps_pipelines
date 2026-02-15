9327e343757d86767125089f1baf834f7bb5d238
biplob <110578485+bks1984@users.noreply.github.com>
2026-02-15 17:54:40 +0530

    Change repo_id for model upload in hosting.py

--- mlops-pipelines/hosting/hosting.py
+++ mlops-pipelines/hosting/hosting.py
@@ -5,7 +5,7 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 
 api.upload_folder(
     folder_path="mlops-pipelines/deployment",
-    repo_id="bipjack/PIMA-Diabetes-Prediction",
+    repo_id="bipjack/ml-healthcare",
     repo_type="space",
     path_in_repo="."
 )
